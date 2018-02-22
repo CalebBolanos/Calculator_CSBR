2a5d126388212d8e9bc2ac4ea01c7b71a06eda35
Caleb Bolaños <bolanos.c@hotmail.com>
2018-02-21 19:46:46 -0600

    Proyecto descomprimido

---
+++ app/src/main/java/mx/cecyt9/ipn/calculator_csbr/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.cecyt9.ipn.calculator_csbr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
